19fdb23d41ea4e6426f7b1edd2dd2c76f64a3bfc
AmandaAlmeidaV <amanda.almeida29062000@gmail.com>
2025-03-05 12:00:24 -0300

    Limpeza do fatorial
    Removido bloco de rotina que não será utilizado

--- UFatorial.pas
+++ UFatorial.pas
@@ -11,7 +11,6 @@ type
     function FatorialIterativo(N: Integer): Int64;
   public
     function CalculaOperacao(A, B: Double): Double; override;
-//    function GerarEquacaoFatorial(N: Integer): String; // Novo método
   end;
 
 implementation
@@ -25,23 +24,6 @@ begin
     Result := Result * i;
 end;
 
-{
-function TFatorial.GerarEquacaoFatorial(N: Integer): String;
-var
-  i: Integer;
-  Equacao: String;
-begin
-  Equacao := IntToStr(N) + '! = ';
-  for i := N downto 1 do
-  begin
-    Equacao := Equacao + IntToStr(i);
-    if i > 1 then
-      Equacao := Equacao + ' · ';
-  end;
-  Equacao := Equacao + ' = ' + IntToStr(FatorialIterativo(N));
-  Result := Equacao;
-end;
-}
 
 function TFatorial.CalculaOperacao(A, B: Double): Double;
 begin
